2038155dee1dfadfed9768d844fbe1f200f4b4b3
John Kristan Torremocha <johnkristan01@gmail.com>
2024-09-24 04:14:00 +0800

    feat: styled the layout of appearance and profile page and added the darkmode config

---
+++ resources/js/Pages/Apperance.vue
@@ -0,0 +1,68 @@
+<script setup>
+  import { onMounted, ref } from 'vue';
+import secureDocsLogo from '../../../public/img/securedocs_logo.png';
+
+
+    onMounted(() => {
+        if(localStorage.getItem('theme') === 'dark'){
+            document.documentElement.classList.add('dark')
+        }
+    });
+ 
+
+    const setLightTheme = () => {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('theme', 'light');
+    }
+
+    const setNightTheme = () => {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+    }
+
+
+    const themes = [
+        {name: "Light Theme", img_src: secureDocsLogo, icon: ['fas', 'sun'], themeSelected: setLightTheme},
+        {name: "Night Theme", img_src: secureDocsLogo, icon: ['fas', 'moon'], themeSelected: setNightTheme},
+    ]
+
+</script>
+
+<template>
+    <div class="flex flex-col w-full items-center ">
+        <div class="relative w-1/2 ">
+            <h1 class="font-semibold text-2xl relative z-10 dark:text-white">Apperance</h1>
+            
+            <div class="absolute inset-0 flex items-center justify-center mt-10">
+                <div class="w-full h-[2px] bg-gray-400 -z-10 "></div>
+            </div>
+        </div>
+
+        <h1 class="mt-8 dark:text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo non, blanditiis aspernatur facilis.</h1>
+
+        <div class="w-1/2 flex justify-around gap-16 gap-5 mt-9 font-semibold">
+            
+            <div 
+                v-for="theme in themes"
+                :key="theme.name"
+                class="flex flex-col items-center w-1/2 h-64 border border-gray-400 p-3 rounded-md hover:shadow-black hover:shadow-md transition-shadow duration-300 hover:cursor-pointer dark:text-white dark:hover:shadow-white hover:shadow-md"
+                @click="theme.themeSelected()"
+            >
+                <div class="flex items-center gap-3">
+                    <h1 class="text-xl">{{ theme.name }}</h1>
+                    <font-awesome-icon 
+                        :icon="theme.icon" 
+                        :class="`text-3xl ${theme.name == 'Light Theme' ? 'text-yellow-600': 'text-gray-400 '}`"
+                    />
+                </div>
+
+                <img :src="theme.img_src" alt="Light Theme Web" width="220">
+            </div>
+
+            
+        </div>
+
+        
+    </div>
+
+</template>
---
+++ resources/js/Pages/Profile.vue
@@ -0,0 +1,52 @@
+<script setup>
+  import secureDocsLogo from '../../../public/img/securedocs_logo.png'
+</script>
+
+<template>
+  <div class="flex flex-col w-full items-center dark:text-white">
+    <div class="relative w-1/2 ">
+      <h1 class="font-semibold text-2xl relative z-10">My Profile</h1>
+      
+      <!-- Underline using pseudo-element -->
+      <div class="absolute inset-0 flex items-center justify-center mt-10">
+        <div class="w-full h-[2px] bg-gray-400 -z-10 dark:bg-gray-600"></div>
+      </div>
+    </div>
+
+    <h1 class="mt-8">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo non, blanditiis aspernatur facilis.</h1>
+
+    <div class="w-1/2 flex justify-around gap-16 gap-5 mt-8 font-semibold">
+        <form class="w-full flex flex-col gap-5 ">
+          <input type="text" class="w-full focus:outline-none border border-gray-200 rounded-md p-2 dark:text-black" placeholder="Full Name">
+
+          <input type="text" class="w-full focus:outline-none border border-gray-200 rounded-md p-2 dark:text-black" placeholder="Full Name">
+          <input type="password" class="w-full focus:outline-none border border-gray-200 rounded-md p-2 dark:text-black" placeholder="Full Name">
+          <input type="password" class="w-full focus:outline-none border border-gray-200 rounded-md p-2 dark:text-black" placeholder="Full Name">
+
+          <button 
+            type="submit" 
+            class="bg-blue-600 rounded-md p-3 text-white hover:opacity-75"
+          >
+            Update Profile
+          </button>
+            
+        </form>
+
+        <div class="relative">
+          <img 
+            :src="secureDocsLogo" 
+            alt="User Profile Picture"
+            class="w-full  rounded-full bg-white p-5"
+          />
+
+          <font-awesome-icon 
+            :icon="['fas', 'pen']" 
+            class="absolute bg-black rounded-full p-3 text-white bottom-[70px] left-[-8px] text-lg dark:text-blue-600"
+          />
+        </div>
+
+        
+    </div>
+
+  </div>
+</template>
