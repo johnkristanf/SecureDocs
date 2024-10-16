48ee1ba34d93b14deeb31a4843b9d7297d4011cf
John Kristan Torremocha <johnkristan01@gmail.com>
2024-10-16 08:58:31 +0800

    feat: styled the recycle bin modal options

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -3,6 +3,7 @@
   import { usePage } from '@inertiajs/vue3';
   import secureDocsLogo from '../../../public/img/securedocs_logo.png';
   import OptionsModal from './OptionsModal.vue';
+  import RecycleBinModal from './RecycleBinModal.vue';
 
   const fileProps = defineProps({
     page: String,
@@ -12,16 +13,26 @@
     },
   });
 
+  console.log("documents: ", ref(fileProps.documents));
+  
+
   const { props } = usePage();
   const user = props.auth.user;
 
-  const showModal = ref(false);
+  const showOptionsModal = ref(false);
+  const showRecycleBinModal = ref(false);
+
   const modalPosition = ref({ top: '0px', left: '0px' });
+
+  const documentId = ref(null);
+  const documentName = ref(null);
+
   const selectedFile = ref(null);
   const ellipsisRef = ref(null);
 
 
-  const handleEllipsisClick = (event, file) => {
+  // Options Modal
+  const handleOptionsModal = (event, file, documentID) => {
     
     const rect = event.currentTarget.getBoundingClientRect();
 
@@ -32,15 +43,56 @@
     
     ellipsisRef.value = event.currentTarget; // Store the ellipsis icon reference
     selectedFile.value = file;  
-    showModal.value = true;    
+    documentId.value = documentID
+    showOptionsModal.value = true;    
 
   };
 
-  const closeModal = () => {
-    showModal.value = false;
+  const closeOptionsModal = () => {
+    showOptionsModal.value = false;
     selectedFile.value = null;
     ellipsisRef.value = null;
   };
+
+
+  // RecycleBin Modal
+
+  const handleRecycleBinModal = (event, document_name, document_id) => {
+    
+    const rect = event.currentTarget.getBoundingClientRect();
+
+    modalPosition.value = {
+      top: `${rect.top + window.scrollY + 20}px`, 
+      left: `${rect.left + window.scrollX - 300}px` 
+    };
+    
+    ellipsisRef.value = event.currentTarget;
+    documentName.value = document_name;  
+    documentId.value = document_id
+    showRecycleBinModal.value = true;    
+
+  };
+
+  const closeRecycleBinModal = () => {
+    showRecycleBinModal.value = false;
+    documentName.value = null;  
+    documentId.value = null
+    ellipsisRef.value = null;
+  };
+
+
+  const formatDate = (dateString) => {
+    const options = { 
+        year: 'numeric', 
+        month: 'short', 
+        day: 'numeric', 
+        hour: 'numeric', 
+        minute: 'numeric', 
+        hour12: true 
+    };
+    return new Date(dateString).toLocaleString('en-US', options);
+};
+
 
 
 
@@ -49,32 +101,46 @@
 <template>
 
   <OptionsModal
-    v-if="showModal"
+    v-if="showOptionsModal"
     :style="{ top: modalPosition.top, left: modalPosition.left }"
     :file="selectedFile"
     :position="modalPosition"
-    @close="closeModal"
+    :document_id="documentId"
+    @close="closeOptionsModal"
     :ellipsisRef="ellipsisRef"
+  />
+
+  <RecycleBinModal 
+    v-if="showRecycleBinModal"
+    :style="{ top: modalPosition.top, left: modalPosition.left }"
+    :position="modalPosition"
+    :document_id="documentId"
+    :document_name="documentName"
+    @close="closeRecycleBinModal"
+    :ellipsisRef="ellipsisRef"
+
   />
 
   <!-- Table structure -->
   <div class="w-full flex justify-end">
-    <div class="flex flex-col w-[80%] mt-7">
+    <div class="flex flex-col w-full md:w-[80%] mt-7">
       <div class="overflow-x-auto shadow-md sm:rounded-lg">
         <div class="inline-block min-w-full align-middle">
-          <div class="flex justify-between items-center w-full mb-4 font-semibold">
+          <div class="flex justify-end items-center w-full mb-4 font-semibold">
 
-            <div v-if="fileProps.page === 'projectFiles'" class="w-1/2 flex items-center gap-10 bg-gray-200 rounded-md p-2">
+            <!-- <div v-if="fileProps.page === 'projectFiles'" class="w-1/2 flex items-center gap-10 bg-gray-200 rounded-md p-2">
               <h1 v-for="type in fileTypeFilter" :key="type.name" :class="[
                   'hover:opacity-75 hover:cursor-pointer',
                   type.active ? 'bg-white p-2 rounded-md' : ''
                 ]">
                 {{ type.name }}
               </h1>
-            </div>
+            </div> -->
+
             <div v-if="fileProps.page === 'projectFiles'" class="w-[30%] mr-5">
               <input type="search" placeholder="Search" class="w-full bg-slate-200 p-2 rounded-md focus:outline-blue-500" />
             </div>
+
           </div>
 
           <!-- Table Body -->
@@ -87,7 +153,7 @@
                     File Name
                   </th>
 
-                  <th v-if="fileProps.page === 'projectFiles'" scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                  <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
                     Uploaded By
                   </th>
                   
@@ -95,7 +161,7 @@
                     {{ fileProps.page === "projectFiles" ? 'Uploaded Date' : 'Deleted Date' }}
                   </th>
 
-                  <th v-if="fileProps.page === 'projectFiles'" scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
+                  <th scope="col" class="py-3 px-6 text-xs font-medium tracking-wider text-left text-gray-700 uppercase dark:text-gray-400">
                   </th>
                 </tr>
               </thead>
@@ -108,7 +174,7 @@
                     {{ document.name }}
                   </td>
 
-                  <td v-if="fileProps.page === 'projectFiles'" class="flex items-center py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                  <td class="flex items-center py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
                     <img class="w-8 h-8 mr-2 rounded-full" :src="secureDocsLogo" alt="logo">
                     <div class="flex flex-col">
                       <h1>{{ user ? user.fullname : 'Unknown User' }}</h1>
@@ -117,13 +183,14 @@
                   </td>
 
                   <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                    {{ document.uploaded_date }}
+                    {{ formatDate(fileProps.page === 'recycleBin' ? document.updated_at : document.created_at) }}
                   </td>
+
 
                   <td class="py-4 px-6 text-lg font-medium text-gray-900 whitespace-nowrap dark:text-white ">
                     <font-awesome-icon 
                       :icon="['fas', 'ellipsis-vertical']" 
-                      @click="handleEllipsisClick($event, document)" 
+                      @click="fileProps.page == 'recycleBin' ? handleRecycleBinModal($event, document.name, document.id) : handleOptionsModal($event, document, document.id)" 
                       class="p-2 rounded-full hover:cursor-pointer hover:opacity-75 hover:bg-gray-300" 
                     />
                   </td>
